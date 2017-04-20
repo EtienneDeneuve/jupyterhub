bbac35d26e8b97b94fa7e9c4998745788ecdb131
littlebigdatacompany <l2c@l2c-conseils.fr>
2017-04-20 22:59:34 +0200

    Update spark_env.sh

--- spark_env.sh
+++ spark_env.sh
@@ -1,5 +1,5 @@
 #!/usr/bin/env bash
-export JAVA_HOME='/usr/local/java/jdk1.8.0_121'
-export JAVA_JRE='$JAVA_HOME/jre'
-export PATH='$PATH:$JRE_HOME/bin:$JAVA_HOME/bin:/usr/local/bin/apache-spark/bin'
-export SPARK_HOME='/usr/local/bin/apache-spark'
+JAVA_HOME='/usr/local/java/jdk1.8.0_121'
+JAVA_JRE='$JAVA_HOME/jre'
+PATH='$PATH:$JRE_HOME/bin:$JAVA_HOME/bin:/usr/local/bin/apache-spark/bin'
+SPARK_HOME='/usr/local/bin/apache-spark'
